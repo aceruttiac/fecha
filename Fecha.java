446c52556829ecd8de15093e9270f626972f6e48
augusto <augustocerutti12@gmail.com>
2026-05-04 15:04:51 -0300

    aa

---
+++ Fecha.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Fecha here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fecha
+{
+    private int dia;
+    private int mes;
+    private int anho;
+    
+    //CORREGIR TODO 
+    /**
+     * Constructor de la clase Fecha
+     */
+    public Fecha (int dia, int mes, int anho){
+        //precondicion y declaracion mia 
+        assert dia < 15 && mes < 10 && anho < 1582: "valor no valido";  
+        
+        //condicional mio
+        //if (anho > 1582) {
+          //  ;
+        //}
+        //else {
+         //   ;
+        //}    
+        //dia = 0;
+        //mes = 0;
+        //anho = 0;
+        
+        
+        //precondicion y declaracion del profe
+        //assert fechaValida;  
+    
+    }
+    
+    //private boolean fechaValida (int anho, int mes, int dia){
+        //return !(anho < 1582 || mes <= 0 || mes >= 12 || dia < )
+    }
+
+
+//}
